cc27aec32909a12109d224374ec84f60e335fb1b
Serednii Mykola <seredniimykola@gmail.com>
2024-06-02 23:09:31 +0200

    change  Sort

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { setSort } from '../redux/filter/slice';
+import { Sort as SortType, SortPropertyEnum } from '../redux/filter/types';
 
+type SortItem = {
+  name: string;
+  sortProperty: SortPropertyEnum;
+};
 
+type PopupClick = MouseEvent & {
+  path: Node[];
+};
 
-export const Sort: React.FC = React.memo(() => {
-  // const dispatch = useDispatch();
+type SortPopupProps = {
+  value: SortType;
+};
+
+export const sortList: SortItem[] = [
+  { name: 'популярности (DESC)', sortProperty: SortPropertyEnum.RATING_DESC },
+  { name: 'популярности (ASC)', sortProperty: SortPropertyEnum.RATING_ASC },
+  { name: 'цене (DESC)', sortProperty: SortPropertyEnum.PRICE_DESC },
+  { name: 'цене (ASC)', sortProperty: SortPropertyEnum.PRICE_ASC },
+  { name: 'алфавиту (DESC)', sortProperty: SortPropertyEnum.TITLE_DESC },
+  { name: 'алфавиту (ASC)', sortProperty: SortPropertyEnum.TITLE_ASC },
+];
+
+export const Sort: React.FC<SortPopupProps> = React.memo(({ value }) => {
+  const dispatch = useDispatch();
   const sortRef = React.useRef<HTMLDivElement>(null);
 
   const [open, setOpen] = React.useState(false);
 
+  const onClickListItem = (obj: SortItem) => {
+    dispatch(setSort(obj));
+    setOpen(false);
+  };
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const _event = event as PopupClick;
+      const path = _event.composedPath ? _event.composedPath() : (_event.path || []);
+      if (sortRef.current && !path.includes(sortRef.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener('click', handleClickOutside);
+    return () =>{
+      console.log('didmount')
+      document.body.removeEventListener('click', handleClickOutside);
+    } 
+  }, []);
 
   return (
     <div ref={sortRef} className="sort">
@@ -25,11 +66,19 @@ export const Sort: React.FC = React.memo(() => {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span onClick={() => setOpen(!open)}></span>
+        <span onClick={() => setOpen(!open)}>{value.name}</span>
       </div>
       {open && (
         <div className="sort__popup">
           <ul>
+            {sortList.map((obj, i) => (
+              <li
+                key={i}
+                onClick={() => onClickListItem(obj)}
+                className={value.sortProperty === obj.sortProperty ? 'active' : ''}>
+                {obj.name}
+              </li>
+            ))}
           </ul>
         </div>
       )}
